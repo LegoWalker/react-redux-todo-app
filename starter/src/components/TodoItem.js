c5f6bb7f74c620d197279e761cb834236344942e
LegoWalker <wayneseegers@gmail.com>
2021-10-22 16:21:24 +1300

    First Changes

--- starter/src/components/TodoItem.js
+++ starter/src/components/TodoItem.js
@@ -15,3 +15,13 @@ const TodoItem = ({ id, title, completed }) => {
 };
 
 export default TodoItem;
+
+{/* <li className={`list-group-item ${completed && 'list-group-item-success'}`}>
+<div className='d-flex justify-content-between'>
+	<span className='d-flex align-items-center'>
+		<input type='checkbox' className='mr-3' checked={completed}></input>
+		{title}
+	</span>
+	<button className='btn btn-danger'>Delete</button>
+</div>
+</li> */}
